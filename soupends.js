29c189ab89ddcbe64635f91d1b76a93ce0e08b8a
falken141 <zm18203@josai.ac.jp>
2020-02-12 19:41:17 +0000

    2/13

--- soupends.js
+++ soupends.js
@@ -1,21 +1,40 @@
 function kekka1(){
-    if(document.getElementById('yourchoice').value=='調味料'||'背表紙'||'小瓶')
+    if(document.getElementById('yourchoice').value =='調味料')
     {
         window.location.href = 'soupends4true.html'; 
     }
 
+    else if (document.getElementById('yourchoice').value =='背表紙')
+    {
+       window.location.href = 'soupends4true.html'; 
+    }
+
+    else if (document.getElementById('yourchoice').value =='小瓶')
+    {
+       window.location.href = 'soupends4true.html'; 
+    }
+
     else{
-        window.location.href = 'soupends4bad.html';
+     window.location.href = 'soupends4bad.html';
     }
 }
-
-function kekka2(){
-    if(document.getElementById('yourschoice').value=='調味料'||'背表紙'||'小瓶')
+    function kekka2(){
+if(document.getElementById('yourschoice').value =='調味料')
     {
         window.location.href = 'soupends5true.html'; 
     }
 
+    else if (document.getElementById('yourschoice').value =='背表紙')
+    {
+       window.location.href = 'soupends5true.html'; 
+    }
+
+    else if (document.getElementById('yourschoice').value =='小瓶')
+    {
+       window.location.href = 'soupends5true.html'; 
+    }
+
     else{
-        window.location.href = 'soupends5bad.html';
+     window.location.href = 'soupends5bad.html';
     }
 }
