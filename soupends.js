5e926b4eb88a3d68b746f07ae482c29531296473
falken141 <zm18203@josai.ac.jp>
2020-02-12 18:24:23 +0000

    2/13

---
+++ soupends.js
@@ -0,0 +1,21 @@
+function kekka1(){
+    if(document.getElementById('yourchoice').value=='調味料'||'背表紙'||'小瓶')
+    {
+        window.location.href = 'soupends4true.html'; 
+    }
+
+    else{
+        window.location.href = 'soupends4bad.html';
+    }
+}
+
+function kekka2(){
+    if(document.getElementById('yourschoice').value=='調味料'||'背表紙'||'小瓶')
+    {
+        window.location.href = 'soupends5true.html'; 
+    }
+
+    else{
+        window.location.href = 'soupends5bad.html';
+    }
+}
